81e1b2530f939a7185b58d85d7bf50c925a7e46c
ananthp77 <ananthp77@gmail.com>
2021-04-01 08:30:59 +0530

    Query Modifications

--- sql/queries/max_orders_by_user.sql
+++ sql/queries/max_orders_by_user.sql
@@ -1,14 +1,20 @@
-SELECT users.id        AS UID,
-       users.name,
-       users.gmail,
-       SUM(orders.amt) total_order
-FROM   orders
-       inner join users
-               ON orders.user_id = users.id
-       inner join vendors
-               ON orders.vendor_id = vendors.id
-WHERE  orders.status = 0
-       AND vendors.is_active = 1
-       AND placed_at >= Date_sub(Now(), interval 1 year)
-GROUP  BY name
-ORDER  BY users.added_at;
+SELECT t.name,
+       t.gmail,
+       t.total_order
+FROM  (SELECT users.id        AS uid,
+              users.name,
+              users.added_at,
+              users.gmail,
+              Sum(orders.amt) total_order
+       FROM   orders
+              INNER JOIN users
+                      ON orders.user_id = users.id
+              INNER JOIN vendors
+                      ON orders.vendor_id = vendors.id
+       WHERE  orders.status = 0
+              AND vendors.is_active = 1
+              AND placed_at >= Date_sub(Now(), INTERVAL 1 year)
+       GROUP  BY name
+       ORDER  BY total_order DESC
+       LIMIT  5) t
+ORDER  BY t.added_at; 
